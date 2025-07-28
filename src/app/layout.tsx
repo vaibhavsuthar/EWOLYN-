f1565814079f85fec4295185a733fc6140646de4
Vaibhav Suthar <vaibhavsuthar9259@gmail.com>
2025-07-28 19:43:50 +0000

    I'm still seeing the Philippine Peso symbol (₱) on the live site instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <meta charSet="UTF-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased" style={{ fontFamily: "'PT Sans', sans-serif" }}>
+      <body className="font-body antialiased" style={{ fontFamily: "'Roboto', sans-serif" }}>
         <Header />
         {children}
         <Footer />
